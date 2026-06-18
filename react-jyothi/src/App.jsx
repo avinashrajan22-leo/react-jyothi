19ffeaea1b972dc2ca5e02d4e22b59944551f08a
avinashrajan22-leo <avinashrajan22@gmail.com>
2026-06-18 15:30:25 +0530

    something

--- react-jyothi/src/App.jsx
+++ react-jyothi/src/App.jsx
@@ -1,26 +1,10 @@
-import {Routes,Route,Link,BrowserRouter } from 'react-router-dom'
-import Homepage from './Components/Homepage'
-import Aboutpage from './Components/Aboutpage'
-import Contactpage from './Components/Contactpage'
+import React from 'react'
+import Form from './Components/Form'
 
 const App = () => {
   return (
     <>
-    <BrowserRouter>
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">about</Link>
-      <Link to="/contact">Contact</Link>
-    </nav>
-
-    <Routes>
-      <Route path="/" element={<Homepage></Homepage>}/>
-      <Route path="/about" element={<Aboutpage></Aboutpage>}/>
-      <Route path="/contact" element={<Contactpage></Contactpage>}/>
-      
-
-    </Routes>
-    </BrowserRouter>
+    <Form></Form>
     </>
   )
 }
--- react-jyothi/src/Components/Form.jsx
+++ react-jyothi/src/Components/Form.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <>
+    <h1>Form task</h1>
+    <form>
+      <label htmlfor="name">Name</label>
+      <input type="text" id="name" name="name"/>
+      <br/>
+      <label htmlfor="email">email</label>
+      <input type="email" id="email" name="email"/>
+      <br/>
+      <label htmlfor="password">password</label>
+      <input type="password" id="password" name="password"/>
+      <br/>
+      <button type="submit">Submit</button>
+    </form>
+    </>
   )
 }
 
